d4a8f63d06c5d31c3dfc40e842adf7c2355ef042
demog <dg08gg@mail.ru>
2022-11-28 16:23:44 +0300

    App.tsx
    Подключил LinkShortener.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import LinkShortener from "./components/LinkShortener";
 
 const App = () => {
   return (
     <div>
-      
+      <LinkShortener />
     </div>
   );
 };
