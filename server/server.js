9e7bc2596a815776c6b7aa63b9f124df0c2e0e18
demog <dg08gg@mail.ru>
2022-11-28 09:01:41 +0300

    server.js / корень сервера
    Подключил router для обработки запросов

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 const express = require("express")
 const cors = require("cors")
+const router = require('./router')
 
 const port = 8000
 const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use('', router)
 
 app.listen(port, () => {
   console.log('Server was start on port: ' + port)
