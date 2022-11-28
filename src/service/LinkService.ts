e24916fa61ae9f5f17f00d59db1b0a8abb80399c
demog <dg08gg@mail.ru>
2022-11-28 10:06:13 +0300

    LinkService.ts / сервис
    Добавил функции для отправки и получения Url'ов

--- src/service/LinkService.ts
+++ src/service/LinkService.ts
@@ -1,9 +1,25 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/dist/query/react'
 
-export const socialAPI = createApi({
+export const linkApi = createApi({
   reducerPath: 'socialAPI',
   baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:8000' }),
   endpoints: (build) => ({
-
+    shortenUrl: build.mutation<string, string>({
+      query: (originalUrl) => ({
+        url: '/links',
+        body: {
+          originalUrl: originalUrl
+        },
+        method: 'POST',
+        responseHandler: response => response.text()
+      })
+    }),
+    getOriginalUrl: build.mutation<string, string>({
+      query: (shortenUrl) => ({
+        url: '/' + shortenUrl,
+        method: 'GET',
+        responseHandler: response => response.text()
+      })
+    })
   })
 })
