4214c6f42ac592c0d15971208751ac0607207440
demog <dg08gg@mail.ru>
2022-11-28 16:25:49 +0300

    LinkService.ts / сервис
    Изменил функции, удалил ненужную

--- src/service/LinkService.ts
+++ src/service/LinkService.ts
@@ -1,24 +1,16 @@
-import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/dist/query/react'
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const linkApi = createApi({
-  reducerPath: 'socialAPI',
+  reducerPath: 'linkApi',
   baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:8000' }),
   endpoints: (build) => ({
-    shortenUrl: build.mutation<string, string>({
+    urlShortener: build.mutation<string, string>({
       query: (originalUrl) => ({
         url: '/links',
         body: {
           originalUrl: originalUrl
         },
-        method: 'POST',
-        responseHandler: response => response.text()
-      })
-    }),
-    getOriginalUrl: build.mutation<string, string>({
-      query: (shortenUrl) => ({
-        url: '/' + shortenUrl,
-        method: 'GET',
-        responseHandler: response => response.text()
+        method: 'POST'
       })
     })
   })
