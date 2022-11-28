1e39e96313e2582439fd06884daef5fa315eeaf9
demog <dg08gg@mail.ru>
2022-11-28 10:06:40 +0300

    store.ts
    Подключил middleware

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,12 +1,16 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import {linkApi} from "../service/LinkService";
 
 const rootReducer = combineReducers({
-
+  [linkApi.reducerPath]: linkApi.reducer
 })
 
 export const setupStore = (): any => {
   return configureStore({
-    reducer: rootReducer
+    reducer: rootReducer,
+    middleware: getDefaultMiddleware =>
+      getDefaultMiddleware()
+        .concat(linkApi.middleware)
   })
 }
 
